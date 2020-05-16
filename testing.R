8ad83dabc8588ef355a5ce35c6c62e776c1801c1
amyfliu <amyfliu2015@gmail.com>
2020-05-16 16:23:55 -0400

    Updated testing.R to test branch

--- testing.R
+++ testing.R
@@ -2,4 +2,4 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 print("Amy")
 
-hello, this is an edit that i made from gitHub to test the "pull" function of Rstudio... hope it works
+hello, this is to test the branch function on Rstudio
