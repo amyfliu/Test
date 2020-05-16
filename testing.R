dd161282fa8673d3ccb73b944b013d1bb36669de
Amy <65267928+amyfliu@users.noreply.github.com>
2020-05-16 16:19:54 -0400

    updated testing.R
    An edit to test the "pull" function of RStudio

--- testing.R
+++ testing.R
@@ -1,3 +1,5 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
 print("Amy")
+
+hello, this is an edit that i made from gitHub to test the "pull" function of Rstudio... hope it works
